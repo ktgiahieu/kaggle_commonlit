bf372e8610f380c2e0b578519b7be535bad9c744
ktgiahieu <ktgiahieu@gmail.com>
2021-07-07 02:13:03 +0000

    update distilbert_regression

---
+++ src/1st_level/distilbert_regression/config.py
@@ -0,0 +1,58 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning) 
+
+import tokenizers
+from transformers import AutoTokenizer
+import os
+is_kaggle = 'KAGGLE_URL_BASE' in os.environ
+
+# Paths
+if is_kaggle:
+    comp_name = 'commonlitreadabilityprize'
+    my_impl = 'commonlit-impl'
+    my_model_dataset = 'commonlit-distilbert-regression'
+
+    TRAINING_FILE = f'../input/{comp_name}/train.csv'
+    TEST_FILE = f'../input/{comp_name}/test.csv'
+    SUB_FILE = f'../input/{comp_name}/sample_submission.csv'
+    MODEL_SAVE_PATH = f'../input/{my_model_dataset}'
+    TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
+    INFERED_PICKLE_PATH = '.'
+
+    MODEL_CONFIG = '../input/distilbert-base-uncased'
+else: #colab
+    repo_name = 'kaggle_commonlit'
+    drive_name = 'Commonlit'
+    model_save = 'distilbert_regression'
+    
+    TRAINING_FILE = f'/content/{repo_name}/data/train_folds.csv'
+    TEST_FILE = f'/content/{repo_name}/data/test.csv'
+    SUB_FILE = f'/content/{repo_name}/data/sample_submission.csv'
+    MODEL_SAVE_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
+    TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
+    INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
+
+    MODEL_CONFIG = 'distilbert-base-uncased'
+
+# Model params
+SEEDS = [25, 42, 123]
+N_FOLDS = 5
+EPOCHS = 4
+LEARNING_RATE = 2e-5
+PATIENCE = None
+EARLY_STOPPING_DELTA = None
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 32
+MAX_LEN = 256  # actually = inf
+
+TOKENIZER = AutoTokenizer.from_pretrained(
+    MODEL_CONFIG)
+
+HIDDEN_SIZE = 768
+N_LAST_HIDDEN = 4
+CLASSIFIER_DROPOUT = 0.1
+WARMUP_RATIO = 0.25
+WEIGHT_DECAY = 0.001
+USE_SWA = False
+SWA_RATIO = 0.9
+SWA_FREQ = 30
