418e9fe97457bf61d9a8b7cc982def15a413f65d
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-11 16:55:01 +0700

    Update models.py

--- src/1st_level/distilroberta_base_regression_v7/models.py
+++ src/1st_level/distilroberta_base_regression_v7/models.py
@@ -6,7 +6,7 @@ import config
 class SelfAttention(torch.nn.Module):
     def __init__(self):
         super(SelfAttention, self).__init__()
-        self.linear1 = torch.nn.Linear(config.HIDDEN_SIZE*2, config.ATTENTION_HIDDEN_SIZE)          
+        self.linear1 = torch.nn.Linear(config.HIDDEN_SIZE, config.ATTENTION_HIDDEN_SIZE)          
         self.tanh = torch.nn.Tanh()            
         self.linear2 = torch.nn.Linear(config.ATTENTION_HIDDEN_SIZE, 1)
         self.softmax = torch.nn.Softmax(dim=1)
@@ -31,20 +31,20 @@ class CommonlitModel(transformers.BertPreTrainedModel):
 
         self.attention = SelfAttention()
 
-        self.linear_compress = torch.nn.Sequential(
-            torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(config.HIDDEN_SIZE*4, 2048)
-        )
+        #self.linear_compress = torch.nn.Sequential(
+        #    torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
+        #    torch.nn.Linear(config.HIDDEN_SIZE*4, 2048)
+        #)
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(2048 + config.N_DOCUMENT_FEATURES, 1)
+            torch.nn.Linear(config.HIDDEN_SIZE + config.N_DOCUMENT_FEATURES, 1)
         )
         
-        for layer in self.linear_compress:
-            if isinstance(layer, torch.nn.Linear):
-                layer.weight.data.normal_(mean=0.0, std=0.02)
-                if layer.bias is not None:
-                    layer.bias.data.zero_()
+        #for layer in self.linear_compress:
+        #    if isinstance(layer, torch.nn.Linear):
+        #        layer.weight.data.normal_(mean=0.0, std=0.02)
+        #        if layer.bias is not None:
+        #            layer.bias.data.zero_()
 
         for layer in self.classifier:
             if isinstance(layer, torch.nn.Linear):
@@ -56,26 +56,25 @@ class CommonlitModel(transformers.BertPreTrainedModel):
         # sequence_output of N_LAST_HIDDEN + Embedding states
         # (N_LAST_HIDDEN + 1, batch_size, num_tokens, 768)
         out = self.automodel(ids, attention_mask=mask)
+        last_hidden_state = out.last_hidden_state
 
         # Mean-max pooler
-        out = out.hidden_states
-        out = torch.stack(
-            tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
-        out_mean = torch.mean(out, dim=0)
-        out_max, _ = torch.max(out, dim=0)
-        pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
+        #out = out.hidden_states
+        #out = torch.stack(
+        #    tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+        #out_mean = torch.mean(out, dim=0)
+        #out_max, _ = torch.max(out, dim=0)
+        #pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
 
         #Self attention on word tokens
-        weights = self.attention(pooled_last_hidden_states[:,1:,:], mask[:,1:])
-        word_vector = torch.sum(weights * pooled_last_hidden_states[:,1:,:], dim=1) 
-        #Concat with CLS tokens
-        context_vector = torch.cat((pooled_last_hidden_states[:,0,:], word_vector), dim=-1)
-        context_vector_compressed = self.linear_compress(context_vector)
+        weights = self.attention(last_hidden_state, mask)
+        context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
+        #context_vector_compressed = self.linear_compress(context_vector)
 
         #Multisample-Dropout
         ##
 
         #Add Document level features
-        context_and_document_vector = torch.cat((context_vector_compressed, document_features), dim=-1)
+        context_and_document_vector = torch.cat((context_vector, document_features), dim=-1)
 
         return self.classifier(context_and_document_vector)
