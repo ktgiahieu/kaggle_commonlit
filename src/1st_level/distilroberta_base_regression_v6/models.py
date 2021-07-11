5a8c09e11df6550142cef2cd1b9108604081ace9
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-11 11:26:55 +0700

    Update models.py

--- src/1st_level/distilroberta_base_regression_v6/models.py
+++ src/1st_level/distilroberta_base_regression_v6/models.py
@@ -31,13 +31,15 @@ class CommonlitModel(transformers.BertPreTrainedModel):
 
         self.attention = SelfAttention()
 
-        self.linear_compress = torch.nn.Sequential(
-            torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(config.HIDDEN_SIZE*4, 128)
-        )
+        #self.linear_compress = torch.nn.Sequential(
+        #    torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
+        #    torch.nn.Linear(config.HIDDEN_SIZE*4, 128)
+        #)
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(128 + config.N_DOCUMENT_FEATURES, 1)
+            torch.nn.Linear(config.HIDDEN_SIZE*4, 2048)
+            torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
+            torch.nn.Linear(config.HIDDEN_SIZE*4, 1)
         )
         
         for layer in self.classifier:
@@ -64,12 +66,12 @@ class CommonlitModel(transformers.BertPreTrainedModel):
         word_vector = torch.sum(weights * pooled_last_hidden_states[:,1:,:], dim=1) 
         #Concat with CLS tokens
         context_vector = torch.cat((pooled_last_hidden_states[:,0,:], word_vector), dim=-1)
-        context_vector_compressed = self.linear_compress(context_vector)
+        #context_vector_compressed = self.linear_compress(context_vector)
 
         #Multisample-Dropout
         ##
 
         #Add Document level features
-        context_and_document_vector = torch.cat((context_vector_compressed, document_features), dim=-1)
+        #context_and_document_vector = torch.cat((context_vector_compressed, document_features), dim=-1)
 
-        return self.classifier(context_and_document_vector)
+        return self.classifier(context_vector)
