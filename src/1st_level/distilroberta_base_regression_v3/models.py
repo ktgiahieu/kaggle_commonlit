e85ac349ddac5e4d6aa312358c5314c14a9011f6
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-11 09:30:52 +0700

    Update models.py

--- src/1st_level/distilroberta_base_regression_v3/models.py
+++ src/1st_level/distilroberta_base_regression_v3/models.py
@@ -14,9 +14,9 @@ class SelfAttention(torch.nn.Module):
     def masked_vector(self, vector, mask):
         if mask is not None:
             mask = mask.float()
-            print( mask.shape,  mask.unsqueeze(1).shape, vector.shape)
+            print( mask.shape,  mask.unsqueeze(-1).shape, vector.shape)
             while mask.dim() < vector.dim():
-                mask = mask.unsqueeze(1)
+                mask = mask.unsqueeze(-1)
             vector = vector + (mask + 1e-45).log()
         return vector
 
