81cd7be15aface2b6d6133d41a00ddf8eb361949
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-12 15:56:43 +0700

    Update models.py

--- src/1st_level/distilroberta_base_regression_v10/models.py
+++ src/1st_level/distilroberta_base_regression_v10/models.py
@@ -51,17 +51,6 @@ class CommonlitModel(transformers.BertPreTrainedModel):
         out = self.automodel(ids, attention_mask=mask)
         last_hidden_state = out.last_hidden_state
 
-        # Mean-max pooler
-        #out = out.hidden_states
-        #out = torch.stack(
-        #    tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
-        #out_mean = torch.mean(out, dim=0)
-        #out_max, _ = torch.max(out, dim=0)
-        #pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
-
-        #Self attention on word tokens
-        #weights = self.attention(last_hidden_state, mask)
-        #context_vector = torch.sum(weights * last_hidden_state, dim=1) 
         context_vector = last_hidden_state[:,0,:]
 
         #Multisample-Dropout
