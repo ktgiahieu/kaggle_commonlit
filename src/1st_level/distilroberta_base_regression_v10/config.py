8e22b71540b7191b81a3f22bf4c89028ee98d24f
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-12 17:26:21 +0700

    Update config.py

--- src/1st_level/distilroberta_base_regression_v10/config.py
+++ src/1st_level/distilroberta_base_regression_v10/config.py
@@ -45,7 +45,7 @@ TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 32
 MAX_LEN = 256  # actually = inf
 MAX_LEN_SENTENCE = 32  # actually = inf
-MAX_N_SENTENCE = 32
+MAX_N_SENTENCE = 24
 
 TOKENIZER = AutoTokenizer.from_pretrained(
     MODEL_CONFIG)
