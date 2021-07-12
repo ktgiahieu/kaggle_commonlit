07c91619634a68a8ed5e214a9b28213cd90b91b5
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-12 15:07:30 +0700

    add

---
+++ src/1st_level/distilroberta_base_regression_v10/dataset.py
@@ -0,0 +1,102 @@
+import numpy as np
+import torch
+import textstat
+
+import config
+
+def rescale_linear(x, minimum, maximum):
+    """Rescale an arrary linearly."""
+    m = 2 / (maximum - minimum)
+    b = -1 - m * minimum
+    return m * x + b
+
+def process_data(text, label,
+                 tokenizer, max_len):
+    """Preprocesses one data sample and returns a dict
+    with targets and other useful info.
+    """
+    sentences = text.split('.')
+    sentences_encoded_dict = tokenizer.batch_encode_plus(
+        sentences,                      # Sentence to encode.
+        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+        max_length = config.MAX_LEN,           # Pad & truncate all sentences.
+        padding = 'max_length',
+		return_attention_mask = True,   # Construct attn. masks.
+        return_tensors = 'pt',     # Return pytorch tensors.
+        truncation = True,
+    )
+    sentences_input_ids = sentences_encoded_dict['input_ids']
+    sentences_mask = sentences_encoded_dict['attention_mask']
+    sentences_features = [rescale_linear(textstat.flesch_reading_ease(text), -40, 120),
+                    rescale_linear(textstat.smog_index(text), -5, 30),
+                    rescale_linear(textstat.flesch_kincaid_grade(text), -5, 30),
+                    rescale_linear(textstat.coleman_liau_index(text), -5, 30),
+                    rescale_linear(textstat.automated_readability_index(text), -5, 30),
+                    rescale_linear(textstat.dale_chall_readability_score(text), 0   , 12),
+                    rescale_linear(textstat.difficult_words(text), 0, 70),
+                    rescale_linear(textstat.linsear_write_formula(text), 0, 30),
+                    rescale_linear(textstat.gunning_fog(text), 0, 30),
+                    rescale_linear(textstat.fernandez_huerta(text), 0, 140),
+                    rescale_linear(textstat.szigriszt_pazos(text), 0, 140),
+                    rescale_linear(textstat.gutierrez_polini(text), 0, 60),
+                    rescale_linear(textstat.crawford(text), -2, -7)]
+
+    encoded_dict = tokenizer.encode_plus(
+        text,                      # Sentence to encode.
+        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+        max_length = config.MAX_LEN,           # Pad & truncate all sentences.
+        padding = 'max_length',
+		return_attention_mask = True,   # Construct attn. masks.
+        return_tensors = 'pt',     # Return pytorch tensors.
+        truncation = True,
+    )
+    # ----------------------------------
+
+    # Input for BERT
+    input_ids = np.squeeze(encoded_dict['input_ids'],0)
+    # Mask of input without padding
+    mask = np.squeeze(encoded_dict['attention_mask'],0)
+
+    document_features = [rescale_linear(textstat.flesch_reading_ease(text), -40, 120),
+                        rescale_linear(textstat.smog_index(text), -5, 30),
+                        rescale_linear(textstat.flesch_kincaid_grade(text), -5, 30),
+                        rescale_linear(textstat.coleman_liau_index(text), -5, 30),
+                        rescale_linear(textstat.automated_readability_index(text), -5, 30),
+                        rescale_linear(textstat.dale_chall_readability_score(text), 0   , 12),
+                        rescale_linear(textstat.difficult_words(text), 0, 70),
+                        rescale_linear(textstat.linsear_write_formula(text), 0, 30),
+                        rescale_linear(textstat.gunning_fog(text), 0, 30),
+                        rescale_linear(textstat.fernandez_huerta(text), 0, 140),
+                        rescale_linear(textstat.szigriszt_pazos(text), 0, 140),
+                        rescale_linear(textstat.gutierrez_polini(text), 0, 60),
+                        rescale_linear(textstat.crawford(text), -2, -7)]
+
+    return {'ids': input_ids,
+            'mask': mask,
+            'labels': [label],
+            'document_features': document_features}
+
+
+class CommonlitDataset:
+    def __init__(self, texts, labels):
+        self.texts = texts
+        self.labels = labels
+        self.max_len = config.MAX_LEN
+        self.tokenizer = config.TOKENIZER
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, item):
+        """Returns preprocessed data sample as dict with
+        data converted to tensors.
+        """
+        data = process_data(self.texts[item],
+                            self.labels[item],
+                            self.tokenizer,
+                            self.max_len)
+
+        return {'ids': torch.tensor(data['ids'], dtype=torch.long),
+                'mask': torch.tensor(data['mask'], dtype=torch.long),
+                'labels': torch.tensor(data['labels'], dtype=torch.float),
+                'document_features': torch.tensor(data['document_features'], dtype=torch.float),}
---
+++ src/1st_level/distilroberta_base_regression_v9/models.py
@@ -0,0 +1,69 @@
+import torch
+import transformers
+
+import config
+
+class SelfAttention(torch.nn.Module):
+    def __init__(self):
+        super(SelfAttention, self).__init__()
+        self.linear1 = torch.nn.Linear(config.HIDDEN_SIZE, config.ATTENTION_HIDDEN_SIZE)          
+        self.tanh = torch.nn.Tanh()            
+        self.linear2 = torch.nn.Linear(config.ATTENTION_HIDDEN_SIZE, 1)
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def masked_vector(self, vector, mask):
+        return vector + (mask.unsqueeze(-1) + 1e-45).log()
+
+    def forward(self, x, mask):
+        out = self.linear1(x)
+        out = self.tanh(out)
+        out = self.linear2(out)
+        out = self.masked_vector(out, mask)
+        out = self.softmax(out)
+        return out
+
+class CommonlitModel(transformers.BertPreTrainedModel):
+    def __init__(self, conf):
+        super(CommonlitModel, self).__init__(conf)
+        self.automodel = transformers.AutoModel.from_pretrained(
+            config.MODEL_CONFIG,
+            config=conf)
+
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
+            torch.nn.Linear(config.HIDDEN_SIZE*2 + config.N_DOCUMENT_FEATURES, 1024),
+            torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
+            torch.nn.Linear(1024, 1),
+        )
+
+        for layer in self.classifier:
+            if isinstance(layer, torch.nn.Linear):
+                layer.weight.data.normal_(mean=0.0, std=0.02)
+                if layer.bias is not None:
+                    layer.bias.data.zero_()
+
+    def forward(self, ids, mask, document_features):
+        # sequence_output of N_LAST_HIDDEN + Embedding states
+        # (N_LAST_HIDDEN + 1, batch_size, num_tokens, 768)
+        out = self.automodel(ids, attention_mask=mask)
+
+        # Mean-max pooler
+        out = out.hidden_states
+        out = torch.stack(
+            tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+        out_mean = torch.mean(out, dim=0)
+        out_max, _ = torch.max(out, dim=0)
+        pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
+
+        #Self attention on word tokens
+        #weights = self.attention(last_hidden_state, mask)
+        #context_vector = torch.sum(weights * last_hidden_state, dim=1) 
+        context_vector = pooled_last_hidden_states[:,0,:]
+
+        #Multisample-Dropout
+        ##
+
+        #Add Document level features
+        context_and_document_vector = torch.cat((context_vector, document_features), dim=-1)
+
+        return self.classifier(context_and_document_vector)
