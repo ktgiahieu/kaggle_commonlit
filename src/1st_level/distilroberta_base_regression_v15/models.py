579e3cb44571abcf05e7b8d1e61b3ceb55a18c40
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-14 00:39:22 +0700

    add

--- src/1st_level/distilroberta_base_regression_v15/models.py
+++ src/1st_level/distilroberta_base_regression_v15/models.py
@@ -4,9 +4,9 @@ import transformers
 import config
 
 class SelfAttention(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, input_size):
         super(SelfAttention, self).__init__()
-        self.linear1 = torch.nn.Linear(config.HIDDEN_SIZE, config.ATTENTION_HIDDEN_SIZE)          
+        self.linear1 = torch.nn.Linear(input_size, config.ATTENTION_HIDDEN_SIZE)          
         self.tanh = torch.nn.Tanh()            
         self.linear2 = torch.nn.Linear(config.ATTENTION_HIDDEN_SIZE, 1)
         self.softmax = torch.nn.Softmax(dim=1)
@@ -29,7 +29,9 @@ class CommonlitModel(transformers.BertPreTrainedModel):
             config.MODEL_CONFIG,
             config=conf)
 
-        self.attention = SelfAttention()
+        self.attention_sent1 = SelfAttention(config.HIDDEN_SIZE*2)
+        self.attention_sent2 = SelfAttention(config.HIDDEN_SIZE*2)
+        self.attention_doc = SelfAttention(config.HIDDEN_SIZE*2)
 
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
@@ -53,24 +55,42 @@ class CommonlitModel(transformers.BertPreTrainedModel):
         for sent_ids, sent_mask, sent_features, sent_attention_mask in zip(sentences_ids, sentences_mask, sentences_features, sentences_attention_mask):
             sent_out = self.automodel(sent_ids, attention_mask=sent_mask)
 
-            #get last hidden state <CLS> vector
-            sent_last_hidden_state = sent_out.last_hidden_state
-            sent_context_vector = sent_last_hidden_state[:,0,:]
+            # Mean-max pooler
+            sent_out = sent_out.hidden_states
+            sent_out = torch.stack(
+                tuple(sent_out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+            sent_out_mean = torch.mean(sent_out, dim=0)
+            sent_out_max, _ = torch.max(sent_out, dim=0)
+            sent_pooled_last_hidden_states = torch.cat((sent_out_mean, sent_out_max), dim=-1)
+
+            #Self attention
+            weights1 = self.attention_sent1(sent_pooled_last_hidden_states, sent_mask)
+            sent_context_vector = torch.sum(weights1 * sent_pooled_last_hidden_states, dim=1) 
 
             #concat external features
             #context_and_sentence_vector = torch.cat((sent_context_vector, sent_features), dim=-1)
 
             #Self attention
-            weights = self.attention(sent_context_vector, sent_attention_mask)
-            sent_attentioned_vector = torch.sum(weights * sent_context_vector, dim=0) 
+            weights2 = self.attention_sent2(sent_context_vector, sent_attention_mask)
+            sent_attentioned_vector = torch.sum(weights2 * sent_context_vector, dim=0) 
             sentences_vector.append(sent_attentioned_vector)
             
         sentences_vector = torch.stack(sentences_vector, dim=0)
 
         #The hold excerpt
         out = self.automodel(ids, attention_mask=mask)
-        last_hidden_state = out.last_hidden_state
-        context_vector = last_hidden_state[:,0,:]
+
+        # Mean-max pooler
+        out = out.hidden_states
+        out = torch.stack(
+            tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+        out_mean = torch.mean(out, dim=0)
+        out_max, _ = torch.max(out, dim=0)
+        pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
+
+        #Self attention
+        weights = self.attention_doc(pooled_last_hidden_states, mask)
+        context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
 
         #Multisample-Dropout
         ##
