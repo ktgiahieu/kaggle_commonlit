48b66ee60b162d8b9601d0644411be9274da5531
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-15 23:00:33 +0700

    add

--- src/1st_level/deberta_large_regression_v1/models.py
+++ src/1st_level/deberta_large_regression_v1/models.py
@@ -24,69 +24,6 @@ class SelfAttention(torch.nn.Module):
 
 class CommonlitModel(transformers.BertPreTrainedModel):
     def __init__(self, conf):
-        conf.num_labels  = 1
-        super(CommonlitModel, self).__init__(conf)
-        self.automodel = transformers.AutoModelForSequenceClassification.from_pretrained(
-            config.MODEL_CONFIG,
-            config=conf)
-
-        #self.attention = SelfAttention()
-
-        #self.classifier = torch.nn.Sequential(
-        #    torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-        #    torch.nn.Linear(config.HIDDEN_SIZE*2, 1),
-        #)
-
-    def forward(self, ids, mask):
-        out = self.automodel(ids, attention_mask=mask)
-
-        # Mean-max pooler
-        #out = out.hidden_states
-        #out = torch.stack(
-        #    tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
-        #out_mean = torch.mean(out, dim=0)
-        #out_max, _ = torch.max(out, dim=0)
-        #pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
-
-        #Self attention
-        #weights = self.attention(pooled_last_hidden_states, mask)
-        #context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
-
-        #return self.classifier(context_vector)
-        return out.logits
-
-
-
-
-
-
-"""
-import torch
-import transformers
-
-import config
-
-class SelfAttention(torch.nn.Module):
-    def __init__(self):
-        super(SelfAttention, self).__init__()
-        self.linear1 = torch.nn.Linear(config.HIDDEN_SIZE*2, config.ATTENTION_HIDDEN_SIZE*2)          
-        self.tanh = torch.nn.Tanh()            
-        self.linear2 = torch.nn.Linear(config.ATTENTION_HIDDEN_SIZE*2, 1)
-        self.softmax = torch.nn.Softmax(dim=1)
-
-    def masked_vector(self, vector, mask):
-        return vector + (mask.unsqueeze(-1) + 1e-45).log()
-
-    def forward(self, x, mask):
-        out = self.linear1(x)
-        out = self.tanh(out)
-        out = self.linear2(out)
-        out = self.masked_vector(out, mask)
-        out = self.softmax(out)
-        return out
-
-class CommonlitModel(transformers.BertPreTrainedModel):
-    def __init__(self, conf):
         super(CommonlitModel, self).__init__(conf)
         self.automodel = transformers.AutoModel.from_pretrained(
             config.MODEL_CONFIG,
@@ -115,4 +52,3 @@ class CommonlitModel(transformers.BertPreTrainedModel):
         context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
 
         return self.classifier(context_vector)
-"""
