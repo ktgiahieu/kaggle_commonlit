94544ec8a10ae4babd9eb7d2b4f48e8612907e9c
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-16 22:04:27 +0700

    add

--- src/1st_level/roberta_large_regression_v4/engine.py
+++ src/1st_level/roberta_large_regression_v4/engine.py
@@ -12,16 +12,14 @@ def loss_fn(outputs, labels):
 
 
 def train_fn(train_data_loader, valid_data_loader, model, optimizer, device, writer, model_path, scheduler=None):
-    losses = utils.AverageMeter()
-
-
-
     best_val_rmse = None
     step = 0
     last_eval_step = 0
     eval_period = config.EVAL_SCHEDULE[0][1]   
     for epoch in range(config.EPOCHS):
+        losses = utils.AverageMeter()
         tk0 = tqdm.tqdm(train_data_loader, total=len(train_data_loader))
+        model.zero_grad()
         for bi, d in enumerate(tk0):
             ids = d['ids']
             mask = d['mask']
@@ -32,37 +30,42 @@ def train_fn(train_data_loader, valid_data_loader, model, optimizer, device, wri
             labels = labels.to(device, dtype=torch.float)
 
             model.train()
-            model.zero_grad()
+            
             outputs = \
                 model(ids=ids, mask=mask)
         
             loss = loss_fn(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
 
             losses.update(loss.item(), ids.size(0))
             tk0.set_postfix(loss=np.sqrt(losses.avg))
 
-            if step >= last_eval_step + eval_period:
-                val_rmse = eval_fn(valid_data_loader, model, device, epoch*len(train_data_loader) + bi, writer)                           
-                last_eval_step = step
-                for rmse, period in config.EVAL_SCHEDULE:
-                    if val_rmse >= rmse:
-                        eval_period = period
-                        break                               
+            loss = loss / config.ACCUMULATION_STEPS   
+            loss.backward()
+
+
+
+            if (bi+1) % config.ACCUMULATION_STEPS    == 0:             # Wait for several backward steps
+                optimizer.step()                            # Now we can do an optimizer step
+                scheduler.step()
+                model.zero_grad()                           # Reset gradients tensors
+                if step >= last_eval_step + eval_period:
+                    val_rmse = eval_fn(valid_data_loader, model, device, epoch*len(train_data_loader) + bi, writer)                           
+                    last_eval_step = step
+                    for rmse, period in config.EVAL_SCHEDULE:
+                        if val_rmse >= rmse:
+                            eval_period = period
+                            break                               
                 
-                if not best_val_rmse or val_rmse < best_val_rmse:                    
-                    best_val_rmse = val_rmse
-                    best_epoch = epoch
-                    torch.save(model.state_dict(), model_path)
-                    print(f"New best_val_rmse: {best_val_rmse:0.4}")
-                else:       
-                    print(f"Still best_val_rmse: {best_val_rmse:0.4}",
-                            f"(from epoch {best_epoch})")                                    
-                    
+                    if not best_val_rmse or val_rmse < best_val_rmse:                    
+                        best_val_rmse = val_rmse
+                        best_epoch = epoch
+                        torch.save(model.state_dict(), model_path)
+                        print(f"New best_val_rmse: {best_val_rmse:0.4}")
+                    else:       
+                        print(f"Still best_val_rmse: {best_val_rmse:0.4}",
+                                f"(from epoch {best_epoch})")                                    
             step += 1
-            
+
         writer.add_scalar('Loss/train', np.sqrt(losses.avg), (epoch+1)*len(train_data_loader))
 
         rmse_score = eval_fn(valid_data_loader, model, device, (epoch+1)*len(train_data_loader), writer)
