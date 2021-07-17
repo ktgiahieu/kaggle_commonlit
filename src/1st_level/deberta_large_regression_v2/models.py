cc24d4bf4926922d5026e559b87116cd4d717a35
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-17 16:14:23 +0700

    add

--- src/1st_level/deberta_large_regression_v2/models.py
+++ src/1st_level/deberta_large_regression_v2/models.py
@@ -29,26 +29,30 @@ class CommonlitModel(transformers.BertPreTrainedModel):
             config.MODEL_CONFIG,
             config=conf)
 
-        self.attention = SelfAttention()
+        #self.attention = SelfAttention()
 
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(config.HIDDEN_SIZE*2, 1),
+            torch.nn.Linear(config.HIDDEN_SIZE, config.HIDDEN_SIZE),
+            torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
+            torch.nn.Linear(config.HIDDEN_SIZE, 1),
         )
 
     def forward(self, ids, mask):
         out = self.automodel(ids, attention_mask=mask)
 
         # Mean-max pooler
-        out = out.hidden_states
-        out = torch.stack(
-            tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
-        out_mean = torch.mean(out, dim=0)
-        out_max, _ = torch.max(out, dim=0)
-        pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
+        #out = out.hidden_states
+        #out = torch.stack(
+        #    tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+        #out_mean = torch.mean(out, dim=0)
+        #out_max, _ = torch.max(out, dim=0)
+        #pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
 
         #Self attention
-        weights = self.attention(pooled_last_hidden_states, mask)
-        context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
+        #weights = self.attention(pooled_last_hidden_states[:,0,:], mask)
+        #context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
+
+        context_vector = out.last_hidden_state[:,0,:]
 
         return self.classifier(context_vector)
