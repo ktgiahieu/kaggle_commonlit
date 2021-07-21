62dda66756106f5ddae64c3cada1bcb886caede8
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-21 15:59:49 +0700

    add

--- src/1st_level/deberta_large_v1_mlm/config.py
+++ src/1st_level/deberta_large_v1_mlm/config.py
@@ -5,11 +5,10 @@ import tokenizers
 from transformers import AutoTokenizer
 import os
 is_kaggle = 'KAGGLE_URL_BASE' in os.environ
-
-# Paths
 comp_name = 'commonlitreadabilityprize'
 my_impl = 'commonlit-impl'
-my_model_dataset = 'commonlit-roberta-large-v1-mlm'
+my_model_dataset = 'commonlit-deberta-large-v1-mlm'
+# Paths
 if is_kaggle:
     TRAINING_FILE = f'../input/{comp_name}/train.csv'
     TEST_FILE = f'../input/{comp_name}/test.csv'
@@ -18,11 +17,11 @@ if is_kaggle:
     TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
     INFERED_PICKLE_PATH = '.'
 
-    MODEL_CONFIG = '../input/commonlit-roberta-large-pretrain'
+    MODEL_CONFIG = '../input/commonlit-deberta-large-pretrain'
 else: #colab
     repo_name = 'kaggle_commonlit'
     drive_name = 'Commonlit'
-    model_save = 'roberta_large_v1_mlm'
+    model_save = 'deberta_large_v1_mlm'
     
     TRAINING_FILE = f'/content/{repo_name}/data/train_folds.csv'
     TEST_FILE = f'/content/{repo_name}/data/test.csv'
@@ -31,19 +30,23 @@ else: #colab
     TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
     INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
 
-    MODEL_CONFIG = '/content/clrp_roberta_large'
+    MODEL_CONFIG = '/content/clrp-deberta-large'
+
 
 # Model params
 SEEDS = [1000, 25, 42]
 N_FOLDS = 5
-EPOCHS = 3
+EPOCHS = 4
 
 PATIENCE = None
 EARLY_STOPPING_DELTA = None
-TRAIN_BATCH_SIZE = 8
-VALID_BATCH_SIZE = 8
-ACCUMULATION_STEPS = 1
+TRAIN_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 4
+ACCUMULATION_STEPS = 2
 MAX_LEN = 248  # actually = inf
+
+TOKENIZER = AutoTokenizer.from_pretrained(
+    MODEL_CONFIG)
 
 EVAL_SCHEDULE = [
                 (0.6, 70*ACCUMULATION_STEPS),
@@ -53,10 +56,6 @@ EVAL_SCHEDULE = [
                 (0.47, 2*ACCUMULATION_STEPS), 
                 (-1., 1*ACCUMULATION_STEPS)
                 ]
-
-
-TOKENIZER = AutoTokenizer.from_pretrained(
-    MODEL_CONFIG)
 
 HIDDEN_SIZE = 1024
 ATTENTION_HIDDEN_SIZE = 1024
@@ -73,5 +72,5 @@ SHOW_ITER_VAL = False
 NUM_SHOW_ITER = 20
 
 #Author hyperparams
-LEARNING_RATES = [2e-5, 3e-5, 4e-5]
+LEARNING_RATES = [5e-6, 8e-6, 1e-5]
 WEIGHT_DECAY = 0.01
