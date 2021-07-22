755437d81b56226588031c03d9b1d49c060666d3
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-22 17:32:09 +0700

    Update utils.py

--- src/1st_level/bart_large_double_attention/utils.py
+++ src/1st_level/bart_large_double_attention/utils.py
@@ -83,4 +83,5 @@ def create_optimizer(model):
                            "weight_decay": weight_decay,
                            "lr": lr})
         last_lr = lr
+        print(name, lr)
     return torch.optim.AdamW(parameters)
