ee1c5fcd30e257cb2bd892651716818bdf0a394f
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-24 00:01:41 +0700

    Update config.py

--- src/1st_level/roberta_large_double_attention_all/config.py
+++ src/1st_level/roberta_large_double_attention_all/config.py
@@ -23,7 +23,7 @@ if is_kaggle:
 else: #colab
     repo_name = 'kaggle_commonlit'
     drive_name = 'Commonlit'
-    model_save = 'roberta_large_double_attention-all'
+    model_save = 'roberta_large_double_attention_all'
     
     TRAINING_FILE = f'/content/{repo_name}/data/train_folds.csv'
     TEST_FILE = f'/content/{repo_name}/data/test.csv'
