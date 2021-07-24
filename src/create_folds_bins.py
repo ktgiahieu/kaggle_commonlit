31359e0f01dae4f8ab5b93bd703526ac526d3501
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-24 08:55:07 +0700

    add

---
+++ src/create_folds_bins.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import KFold, StratifiedKFold
+class ContinuousStratifiedKFold(StratifiedKFold):
+    def split(selfself, x, y, groups=None):
+        num_bins = int(np.floor(1 + np.log2(len(y))))
+        bins = pd.cut(y, bins=num_bins, labels=False)
+        return super().split(x, bins, groups)
+
+if __name__ == "__main__":
+    df = pd.read_csv("./data/train.csv")
+    #df = df.dropna().reset_index(drop=True)
+    df["kfold"] = -1
+
+    df = df.sample(frac=1, random_state=50898).reset_index(drop=True)
+
+    #kf = model_selection.KFold(n_splits=5)
+    kf = ContinuousStratifiedKFold(n_splits=5) #shuffle=True, random_state=SEED
+
+    for fold, (trn_, val_) in enumerate(kf.split(X=df, y=df.target.values)):
+        print(len(trn_), len(val_))
+        df.loc[val_, 'kfold'] = fold
+
+    df.to_csv("./data/train_folds_bins.csv", index=False)
+    
+
