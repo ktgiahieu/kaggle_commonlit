26cb32a22206fc3374b974e6ce6c9a68c5d2c7d3
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-25 14:05:09 +0700

    add

--- src/1st_level/deberta_v2_xlarge_meanmax/models.py
+++ src/1st_level/deberta_v2_xlarge_meanmax/models.py
@@ -6,9 +6,9 @@ import config
 class SelfAttention(torch.nn.Module):
     def __init__(self):
         super(SelfAttention, self).__init__()
-        self.linear1 = torch.nn.Linear(config.HIDDEN_SIZE*2, config.ATTENTION_HIDDEN_SIZE*2)          
+        self.linear1 = torch.nn.Linear(config.HIDDEN_SIZE, config.ATTENTION_HIDDEN_SIZE)          
         self.tanh = torch.nn.Tanh()            
-        self.linear2 = torch.nn.Linear(config.ATTENTION_HIDDEN_SIZE*2, 1)
+        self.linear2 = torch.nn.Linear(config.ATTENTION_HIDDEN_SIZE, 1)
         self.softmax = torch.nn.Softmax(dim=1)
 
     def masked_vector(self, vector, mask):
@@ -33,23 +33,23 @@ class CommonlitModel(transformers.BertPreTrainedModel):
 
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(config.HIDDEN_SIZE*2, 1),
+            torch.nn.Linear(config.HIDDEN_SIZE, 1),
         )
 
     def forward(self, ids, mask):
         out = self.automodel(ids, attention_mask=mask)
 
         ## Mean-max pooler
-        out = out.hidden_states
-        out = torch.stack(
-            tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
-        out_mean = torch.mean(out, dim=0)
-        out_max, _ = torch.max(out, dim=0)
-        pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
-        #last_hidden_state = out.last_hidden_state
+        #out = out.hidden_states
+        #out = torch.stack(
+        #    tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+        #out_mean = torch.mean(out, dim=0)
+        #out_max, _ = torch.max(out, dim=0)
+        #pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
+        last_hidden_state = out.last_hidden_state
 
         #Self attention
-        weights = self.attention(pooled_last_hidden_states, mask)
-        context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
+        weights = self.attention(last_hidden_state, mask)
+        context_vector = torch.sum(weights * last_hidden_state, dim=1) 
 
         return self.classifier(context_vector)
