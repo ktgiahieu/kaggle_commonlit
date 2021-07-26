f401ae88f015ae7d481a53653fae74407ca0ff90
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-26 09:06:45 +0700

    Update config.py

--- src/1st_level/roberta_large_v1_nli/config.py
+++ src/1st_level/roberta_large_v1_nli/config.py
@@ -32,7 +32,7 @@ else: #colab
     TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
     INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
 
-    MODEL_CONFIG = '/content/roberta-large-nli'
+    MODEL_CONFIG = '/content/roberta-large-nli-one-model'
 
 # Model params
 SEEDS = [1000, 25, 42]
