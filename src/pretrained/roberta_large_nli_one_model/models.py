49d1e3fad647eb40a0277261b4b07db6ecf091a4
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-26 07:44:14 +0700

    Update models.py

--- src/pretrained/roberta_large_nli_one_model/models.py
+++ src/pretrained/roberta_large_nli_one_model/models.py
@@ -33,7 +33,7 @@ class CommonlitModel(transformers.BertPreTrainedModel):
 
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(config.HIDDEN_SIZE*2, 1),
+            torch.nn.Linear(config.HIDDEN_SIZE, 1),
             torch.nn.Sigmoid(),
         )
 
@@ -71,15 +71,15 @@ class CommonlitModel(transformers.BertPreTrainedModel):
 
         #context_vector = torch.cat([context_vector_x, context_vector_y], dim=-1)
 
-        out = out.hidden_states
-        out = torch.stack(
-            tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
-        out_mean = torch.mean(out, dim=0)
-        out_max, _ = torch.max(out, dim=0)
-        pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
+        #out = out.hidden_states
+        #out = torch.stack(
+        #    tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+        #out_mean = torch.mean(out, dim=0)
+        #out_max, _ = torch.max(out, dim=0)
+        #pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
 
-        #self attention
-        weights = self.attention(pooled_last_hidden_states, mask)
-        context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
-
+        ##self attention
+        #weights = self.attention(pooled_last_hidden_states, mask)
+        #context_vector = torch.sum(weights * pooled_last_hidden_states, dim=1) 
+        context_vector = out.last_hidden_state[:,0,:]
         return self.classifier(context_vector)
