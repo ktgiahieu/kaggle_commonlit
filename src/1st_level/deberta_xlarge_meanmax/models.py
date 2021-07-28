ad1997a6ed0fe8400696e239334b00e8fd4bd128
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-28 09:50:21 +0700

    Update models.py

--- src/1st_level/deberta_xlarge_meanmax/models.py
+++ src/1st_level/deberta_xlarge_meanmax/models.py
@@ -33,23 +33,33 @@ class CommonlitModel(transformers.BertPreTrainedModel):
 
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
-            torch.nn.Linear(config.HIDDEN_SIZE, 1),
+            torch.nn.Linear(config.HIDDEN_SIZE*9, config.HIDDEN_SIZE*6),
+            torch.nn.GELU(),
+            torch.nn.Dropout(config.CLASSIFIER_DROPOUT),
+            torch.nn.Linear(config.HIDDEN_SIZE*6, 1),
         )
 
     def forward(self, ids, mask):
         out = self.automodel(ids, attention_mask=mask)
 
-        ## Mean-max pooler
-        #out = out.hidden_states
-        #out = torch.stack(
-        #    tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
-        #out_mean = torch.mean(out, dim=0)
-        #out_max, _ = torch.max(out, dim=0)
-        #pooled_last_hidden_states = torch.cat((out_mean, out_max), dim=-1)
-        last_hidden_state = out.last_hidden_state
+        # Mean-max pooler
+        out = out.hidden_states
+        out = torch.stack(
+            tuple(out[-i - 1] for i in range(config.N_LAST_HIDDEN)), dim=0)
+        out_mean = torch.mean(out, dim=0)
+        out_max, _ = torch.max(out, dim=0)
+        out_std = torch.std(out, dim=0)
+        pooled_last_hidden_states = torch.cat((out_mean, out_max,out_std), dim=-1)
+        
+        #last_hidden_state = out.last_hidden_state
 
-        #Self attention
-        weights = self.attention(last_hidden_state, mask)
-        context_vector = torch.sum(weights * last_hidden_state, dim=1) 
+        ##Self attention
+        #weights = self.attention(last_hidden_state, mask)
+        #context_vector = torch.sum(weights * last_hidden_state, dim=1) 
+
+        out_pooled_mean = torch.mean(pooled_last_hidden_states, dim=1)
+        out_pooled_max, _ = torch.max(pooled_last_hidden_states, dim=1)
+        out_pooled_std = torch.std(pooled_last_hidden_states, dim=1)
+        context_vector = torch.cat([out_pooled_mean, out_pooled_max, out_pooled_std], dim=-1)
 
         return self.classifier(context_vector)
