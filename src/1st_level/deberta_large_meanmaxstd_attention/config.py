4af8dd19239b610330db6f34a839998710e12033
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-29 19:23:27 +0700

    add

---
+++ src/1st_level/deberta_large_meanmaxstd_attention/config.py
@@ -0,0 +1,78 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning) 
+
+import tokenizers
+from transformers import AutoTokenizer
+import os
+is_kaggle = 'KAGGLE_URL_BASE' in os.environ
+
+# Paths
+model_type = 'deberta-large'
+comp_name = 'commonlitreadabilityprize'
+my_impl = 'commonlit-impl'
+my_model_dataset = 'commonlit-deberta-large-meanmaxstd-attention'
+if is_kaggle:
+    TRAINING_FILE = f'../input/{comp_name}/train.csv'
+    TEST_FILE = f'../input/{comp_name}/test.csv'
+    SUB_FILE = f'../input/{comp_name}/sample_submission.csv'
+    MODEL_SAVE_PATH = f'../input/{my_model_dataset}'
+    TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
+    INFERED_PICKLE_PATH = '.'
+
+    MODEL_CONFIG = '../input/deberta-large'
+else: #colab
+    repo_name = 'kaggle_commonlit'
+    drive_name = 'Commonlit'
+    model_save = 'deberta_large_meanmaxstd_attention'
+    
+    TRAINING_FILE = f'/content/{repo_name}/data/train_folds_bins.csv'
+    TEST_FILE = f'/content/{repo_name}/data/test.csv'
+    SUB_FILE = f'/content/{repo_name}/data/sample_submission.csv'
+    MODEL_SAVE_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
+    TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
+    INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
+
+    MODEL_CONFIG = 'deberta-large'
+
+# Model params
+SEEDS = [1000, 25, 42, 123, 456, 789]
+N_FOLDS = 5
+EPOCHS = 4
+
+PATIENCE = None
+EARLY_STOPPING_DELTA = None
+TRAIN_BATCH_SIZE = 2
+VALID_BATCH_SIZE = 4
+ACCUMULATION_STEPS = 1
+MAX_LEN = 248  # actually = inf
+
+TOKENIZER = AutoTokenizer.from_pretrained(
+    MODEL_CONFIG)
+
+EVAL_SCHEDULE = [
+                (0.6, 200*ACCUMULATION_STEPS),
+                (0.50, 96*ACCUMULATION_STEPS), 
+                (0.49, 48*ACCUMULATION_STEPS), 
+                (0.48, 32*ACCUMULATION_STEPS), 
+                (0.47, 24*ACCUMULATION_STEPS), 
+                (-1., 12*ACCUMULATION_STEPS)
+                ]
+
+HIDDEN_SIZE = 1024
+ATTENTION_HIDDEN_SIZE = 1024
+N_LAST_HIDDEN = 4
+BERT_DROPOUT = 0
+CLASSIFIER_DROPOUT = 0
+WARMUP_RATIO = 0.125
+
+USE_SWA = False
+SWA_RATIO = 0.9
+SWA_FREQ = 30
+
+SHOW_ITER_VAL = False
+NUM_SHOW_ITER = 20
+
+#Author hyperparams
+HEAD_LEARNING_RATE = 1e-3
+LEARNING_RATES_RANGE = [3e-6, 8e-6]
+WEIGHT_DECAY = 0.01
---
+++ src/1st_level/deberta_large_meanmaxstd_attention/infer.py
@@ -0,0 +1,95 @@
+import sys
+import pickle
+import os
+import gc
+
+import numpy as np
+import pandas as pd
+import torch
+import transformers
+import tqdm
+
+import config
+import models
+import dataset
+import utils
+
+
+def run():
+    df_test = pd.read_csv(config.TEST_FILE)
+    df_test.loc[:, 'label'] = 0
+    df_test.rename(columns={'excerpt': 'text'}, inplace=True)
+
+    device = torch.device('cuda')
+    model_config = transformers.AutoConfig.from_pretrained(
+        config.MODEL_CONFIG)
+    model_config.output_hidden_states = True
+
+    test_dataset = dataset.CommonlitDataset(
+        texts=df_test.text.values,
+        labels=df_test.label.values)
+
+    data_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        shuffle=False,
+        batch_size=config.VALID_BATCH_SIZE,
+        num_workers=1)
+    
+    predicted_labels = []
+    for i in range(config.N_FOLDS):  
+        all_models = []
+        for seed in config.SEEDS:
+            model = models.CommonlitModel(conf=model_config)
+            model.to(device)
+            if config.is_kaggle:
+                if i<=2:
+                    model_path = f'{config.TRAINED_MODEL_PATH}-p1/model_{i}_{seed}.bin'
+                else:
+                    model_path = f'{config.TRAINED_MODEL_PATH}-p2/model_{i}_{seed}.bin'
+            else:
+                model_path = f'{config.TRAINED_MODEL_PATH}/model_{i}_{seed}.bin'
+            model.load_state_dict(torch.load(model_path), strict=False)
+            model.eval()
+            all_models.append(model)
+
+        predicted_labels_per_fold = []
+        with torch.no_grad():
+            tk0 = tqdm.tqdm(data_loader, total=len(data_loader))
+            for bi, d in enumerate(tk0):
+                ids = d['ids']
+                mask = d['mask']
+                labels = d['labels']
+
+                ids = ids.to(device, dtype=torch.long)
+                mask = mask.to(device, dtype=torch.long)
+                labels = labels.to(device, dtype=torch.float)
+
+
+                outputs_seeds = []
+                for s in range(len(config.SEEDS)):
+                    outputs = \
+                      all_models[s](ids=ids, mask=mask)
+
+                    outputs_seeds.append(outputs)
+
+                outputs = sum(outputs_seeds) / (len(config.SEEDS))
+
+                outputs = outputs.cpu().detach().numpy()
+                predicted_labels_per_fold.extend(outputs.squeeze(-1).tolist())
+        predicted_labels.append(predicted_labels_per_fold)
+        del all_models
+        torch.cuda.empty_cache()
+        gc.collect()
+    predicted_labels = np.mean(np.array(predicted_labels), axis=0).tolist()
+
+    if not os.path.isdir(f'{config.INFERED_PICKLE_PATH}'):
+        os.makedirs(f'{config.INFERED_PICKLE_PATH}')
+        
+    pickle_name = sys.argv[1]
+    with open(f'{config.INFERED_PICKLE_PATH}/{pickle_name}.pkl', 'wb') as handle:
+        pickle.dump(predicted_labels, handle)
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) > 1, "Please specify output pickle name."
+    run()
