3af9155250b026792e600a2b4e1919bf337f74af
Hieu Khuong <ktgiahieu@gmail.com>
2021-07-31 23:16:50 +0700

    add

--- src/1st_level/roberta_large_regression_v1/infer.py
+++ src/1st_level/roberta_large_regression_v1/infer.py
@@ -36,18 +36,20 @@ def run():
         num_workers=1)
     
     predicted_labels = []
+    all_models = []
+    for seed in config.SEEDS:
+        model = models.CommonlitModel(conf=model_config)
+        model.to(device)
+        model.eval()
+        all_models.append(model)
     for i in range(config.N_FOLDS):  
         all_models = []
         torch.cuda.empty_cache()
         gc.collect()
-        for seed in config.SEEDS:
-            model = models.CommonlitModel(conf=model_config)
-            model.to(device)
-            model.load_state_dict(torch.load(
+        for s, seed in enumerate(config.SEEDS):
+            all_models[s].load_state_dict(torch.load(
                 f'{config.TRAINED_MODEL_PATH}/model_{i}_{seed}.bin'),
                 strict=False)
-            model.eval()
-            all_models.append(model)
 
         predicted_labels_per_fold = []
         with torch.no_grad():
@@ -74,9 +76,6 @@ def run():
                 outputs = outputs.cpu().detach().numpy()
                 predicted_labels_per_fold.extend(outputs.squeeze(-1).tolist())
         predicted_labels.append(predicted_labels_per_fold)
-        del all_models
-        torch.cuda.empty_cache()
-        gc.collect()
     predicted_labels = np.mean(np.array(predicted_labels), axis=0).tolist()
 
     if not os.path.isdir(f'{config.INFERED_PICKLE_PATH}'):
@@ -85,6 +84,12 @@ def run():
     pickle_name = sys.argv[1]
     with open(f'{config.INFERED_PICKLE_PATH}/{pickle_name}.pkl', 'wb') as handle:
         pickle.dump(predicted_labels, handle)
+
+    del test_dataset
+    del data_loader
+    del all_models
+    torch.cuda.empty_cache()
+    gc.collect()
 
 
 if __name__ == '__main__':
